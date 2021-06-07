2e37019b51725a780360e45fc0d797e6d69b0e4b
sunny <parks38@uw.edu>
2021-06-07 15:00:04 +0900

    feat: revised style and loosely coupled

--- src/main/java/com/project/catcaring/controller/PostController.java
+++ src/main/java/com/project/catcaring/controller/PostController.java
@@ -2,29 +2,56 @@ package com.project.catcaring.controller;
 
 import static com.project.catcaring.error.HttpResponses.*;
 
+import com.project.catcaring.aop.annotation.CheckLogin;
+import com.project.catcaring.aop.annotation.CheckLoginAndResult;
+import com.project.catcaring.aop.annotation.CheckUserLoginAndPostMatch;
+import com.project.catcaring.aop.annotation.CurrentUserId;
+import com.project.catcaring.dto.post.PageRequest;
 import com.project.catcaring.dto.post.PostInfoRequest;
-import com.project.catcaring.service.user.LoginSessionService;
+import com.project.catcaring.dto.post.PostUpdateRequest;
 import com.project.catcaring.service.PostServiceImpl;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/posts")
 public class PostController {
+
   private final PostServiceImpl postService;
-  private final LoginSessionService loginSessionService;
 
   @PostMapping
-  public ResponseEntity<String> savePost( @RequestBody @NonNull PostInfoRequest postInfoRequest) {
-    postService.uploadPost(postInfoRequest, loginSessionService.getCurrentUserId());
+  @CheckLogin
+  public ResponseEntity<String> savePost(@RequestBody @NonNull PostInfoRequest postInfoRequest, @CurrentUserId Long currentUserId) {
+    postService.savePost(postInfoRequest, currentUserId);
     return RESPONSE_OK;
+  }
+
+  @DeleteMapping("/{postId}")
+  @CheckUserLoginAndPostMatch
+  public ResponseEntity<String> deletePost(@PathVariable Long postId, @CurrentUserId Long currentUserId) {
+    postService.deletePost(postId, currentUserId);
+    return RESPONSE_OK;
+  }
+
+  @PatchMapping("/{postId}")
+  @CheckUserLoginAndPostMatch
+  public ResponseEntity<String> updatePost(@RequestBody PostUpdateRequest postUpdateRequest, @PathVariable Long postId, @CurrentUserId Long currentUserId) {
+    postService.updatePost(postUpdateRequest, postId, currentUserId);
+    return RESPONSE_OK;
+  }
+
+  @GetMapping("/{pageId}")
+  public Object viewPostLists(@PathVariable("pageId") int page) {
+    return postService.viewPostLists(PageRequest.findScope(page));
   }
 }
--- src/main/java/com/project/catcaring/domain/Address.java
+++ src/main/java/com/project/catcaring/domain/Address.java
@@ -1,6 +1,8 @@
 package com.project.catcaring.domain;
 
 import com.project.catcaring.domain.User.Location;
+import com.project.catcaring.domain.User.MemberShip;
+import com.project.catcaring.dto.post.PostInfoRequest;
 import lombok.Builder;
 
 @Builder
@@ -10,5 +12,19 @@ public class Address {
   private final Long postId;
   private final Location location;
   private final String locationDetail;
-  private final Authority authorityCode;
+  private final MemberShip authorityCode;
+
+  public static Address generate(PostInfoRequest postInfoRequest, Long postId) {
+    MemberShip authority = postInfoRequest.getLocationAuthorityCode();
+
+    if(authority == null) {
+      authority = MemberShip.ACTIVE_MEM;
+    }
+
+    return Address.builder()
+        .postId(postId)
+        .location(postInfoRequest.getLocation())
+        .locationDetail(postInfoRequest.getLocationDetail())
+        .authorityCode(authority).build();
+  }
 }
