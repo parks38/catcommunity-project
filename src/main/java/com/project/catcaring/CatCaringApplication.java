8863a9ef4d48a46afc32722b79c41c71f3cffdf0
sunny <parks38@uw.edu>
2021-07-14 23:54:04 +0900

    feat: redis prep 환경 설정

--- src/main/java/com/project/catcaring/CatCaringApplication.java
+++ src/main/java/com/project/catcaring/CatCaringApplication.java
@@ -2,8 +2,10 @@ package com.project.catcaring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class CatCaringApplication {
 
   public static void main(String[] args) {
---
+++ src/main/java/com/project/catcaring/config/RedisConfig.java
@@ -0,0 +1,5 @@
+package com.project.catcaring.config;
+
+public class RedisConfig {
+
+}
